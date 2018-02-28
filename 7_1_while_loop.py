93a8f2c0bd5b8e0b69a41abdbb2aad4c13869712
mohammad hossain <you@example.com>
2018-02-28 06:37:51 +0330

    loops examples added

---
+++ 7_1_while_loop.py
@@ -0,0 +1,8 @@
+##################################
+## example of a simple while loop#
+##################################
+a=input("please enter your name\n")
+while a != "Ali":
+    print("oops you are not a legal user")
+    a = input("please enter your name\n")
+print("thanks for your registration")
