52c3bb64def81eb97a8f1ed3b06455afe83e7aaf
Javad Heidari <36712713+hejavad@users.noreply.github.com>
2018-03-03 16:45:19 +0330

    Update 2_1_strings.py
    تغییر سلام به hello 
    تغییر نام اضافه کردن متغیر فامیل  و اضافه کردن فامیل به گریتینگ ها 
    
    از کامنت خارج کردن استفاده از متد ها 
    پاک کردن عدد پی و تعریف متغیر جدیدی به نام عدد
    
    جواد حیدری 39301861

--- 2_1_strings.py
+++ 2_1_strings.py
@@ -2,32 +2,33 @@
 # following variables are the same #
 ####################################
 # str1=" salam "
-# str2='salam'
+ str2='Hello'
 
 ######################
 ## concat two string #
 ######################
-# name='mohammad'
-# greeting= str1+name
-# print(greeting)
-#
-# greeting2=str1+" "+name
-# print(greeting2)
-#
-# print(("\'"+greeting2+" \'") * 3)
+ name='Javad'
+ family='heidari'
+ greeting= str1+name+family
+ print(greeting)
+
+ greeting2=str1+" "+name+" "+family
+ print(greeting2)
+
+ print(("\'"+greeting2+" \'") * 3)
 ######################################
 ## some useful operations on strings #
 ######################################
-# print(greeting2.capitalize())
-# print(greeting2.upper())
-# print(greeting2.rsplit(' ')) # splitting the string by blank
-# print(greeting2.strip(' ')) # remove the blanks before and after the string
+ print(greeting2.capitalize())
+ print(greeting2.upper())
+ print(greeting2.rsplit(' ')) # splitting the string by blank
+ print(greeting2.strip(' ')) # remove the blanks before and after the string
 
 ###########################################
 ### another sample of printing a varibale #
 ###########################################
-pi = 3.14
-print(pi)
-str_pi = str(pi)
-print("pi number is : ", pi)  # print each part of input separately
-print("pi number is : " + str(pi))  # concat two string
+adad = 5
+print(adad)
+str_adad = str(adad)
+print("adad number is : ", adad)  # print each part of input separately
+print("adad number is : " + str(adad))  # concat two string
