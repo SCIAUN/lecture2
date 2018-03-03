e60b5d5d335a2a580d326b56a287417aec9297f2
Javad Heidari <36712713+hejavad@users.noreply.github.com>
2018-03-03 17:00:53 +0330

    Update 5_1_logic_operator_on_booleans.py
    تبدیل کد بالا از دو متغیره بودن به سه متغیره

--- 5_1_logic_operator_on_booleans.py
+++ 5_1_logic_operator_on_booleans.py
@@ -1,17 +1,24 @@
 a= True
-b= False
+b= True
+c= False
 print("a is {0}".format(a))
 print("b is {0}".format(b))
+print("b is {0}".format(c))
 print("~a is {0}".format(not a))
 print("~b is {0}".format(not b))
-print("a & b is {0}".format(a and b)) # True if both of them are True
-print("a or b is {0}".format(a or b)) # True if one or both of them are True
+print("~b is {0}".format(not c))
+print("a & b is {0}".format(a and b and c)) 
+print("a or b is {0}".format(a or b or c)) 
 
 
 #################################################
-## A     ### B     ### A and B ### A or B #######
-## True  ### True  ### True    ### True   #######
-## True  ### False ### False   ### True   #######
-## False ### True  ### False   ### True   #######
-## False ### False ### False   ### False  #######
+## A     ### B     ### c      ### A and B and c ### A or B or c  #######
+## True  ### True  ### True   ### True          ### True         #######
+## True  ### True  ### False  ### False         ### True         #######
+## True  ### False ### True   ### False         ### True         #######
+## True  ### False ### False  ### False         ### True         #######
+## False ### True  ### True   ### False         ### True         #######
+## False ### True  ### False  ### False         ### True         #######
+## False ### False ### True   ### False         ### True         #######
+## False ### False ### False  ### False         ### False        #######
 #################################################
